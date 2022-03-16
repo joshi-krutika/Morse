3c2dc7d50a92d69a9577e337292678c67ec7cfbb
joshi-krutika <86966398+joshi-krutika@users.noreply.github.com>
2022-03-16 16:40:27 +0530

    Add files via upload

---
+++ Morse/javasript.js
@@ -0,0 +1,62 @@
+function decodeMorse(morseCode) {
+  var ref = { 
+    '.-':     'a',
+    '-...':   'b',
+    '-.-.':   'c',
+    '-..':    'd',
+    '.':      'e',
+    '..-.':   'f',
+    '--.':    'g',
+    '....':   'h',
+    '..':     'i',
+    '.---':   'j',
+    '-.-':    'k',
+    '.-..':   'l',
+    '--':     'm',
+    '-.':     'n',
+    '---':    'o',
+    '.--.':   'p',
+    '--.-':   'q',
+    '.-.':    'r',
+    '...':    's',
+    '-':      't',
+    '..-':    'u',
+    '...-':   'v',
+    '.--':    'w',
+    '-..-':   'x',
+    '-.--':   'y',
+    '--..':   'z',
+    '.----':  '1',
+    '..---':  '2',
+    '...--':  '3',
+    '....-':  '4',
+    '.....':  '5',
+    '-....':  '6',
+    '--...':  '7',
+    '---..':  '8',
+    '----.':  '9',
+    '-----':  '0',
+  };
+
+  return morseCode
+    .split('   ')
+    .map(
+      a => a
+        .split(' ')
+        .map(
+          b => ref[b]
+        ).join('')
+    ).join(' ');
+}
+
+
+function add() {
+var decoded = document.getElementById('textboxid').value;
+var decode = decodeMorse(decoded);
+// var d = decode.fontsize(10);
+console.log(decode)
+// document.write(d)
+document.getElementById("output").innerHTML = "Is this working";
+}
+
+
